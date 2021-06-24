52c534c7c2f02822f42a353af6fe89934f1946ca
Jayce Su <36811022+AlexSJJ@users.noreply.github.com>
2021-06-24 11:07:59 +0800

    Add files via upload

---
+++ source/server/java/com/example/frauddetection/FraudDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.frauddetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudDetectionApplication.class, args);
+    }
+
+}
